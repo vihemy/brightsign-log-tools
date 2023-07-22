4069a1ebd02a7079eb65c47b3c863ebb72301f9c
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-22 23:57:43 +0200

    simplifying of variables and methods-names

--- brightsign_scripts/log_mover.py
+++ brightsign_scripts/log_mover.py
@@ -14,45 +14,40 @@ class LogMover:
         self.src_folder = src_folder
         self.dst_parent_folder = dst_parent_folder
 
-    def copy_logs_to_new_directory(self, src_folder, dst_parent_folder):
-        dst_player_folder = self._set_destination_player_folder(
-            dst_parent_folder
-        )
+    def relocate_logs(self):
+        dst_player_folder = self._set_dst_player_folder()
         utilities.create_directory(dst_player_folder)
-        self._copy_unique_files_to_directory(src_folder, dst_player_folder)
+        self._move_non_dublicates(dst_player_folder)
 
-    def _set_destination_player_folder(self, dst_parent_folder):
-        dst_player_folder = dst_parent_folder + f"\{self.player.name}"
+    def _set_dst_player_folder(self):
+        dst_player_folder = self.dst_parent_folder + f"\{self.player.name}"
         return dst_player_folder
 
-    def _copy_unique_files_to_directory(self, src_folder, dst_player_folder):
+    def _move_non_dublicates(self, dst_player_folder):
         nr_files_moved = 0
-        for filename in os.listdir(src_folder):
+        for filename in os.listdir(self.src_folder):
             if (
-                self._does_file_belong_to_player == True
-                and self._is_file_duplicate(filename) == False
+                self._belongs_to_player(filename) == True
+                and self._is_duplicate(filename) == False
             ):
-                shutil.copy(
-                    os.path.join(src_folder, filename), dst_player_folder
-                )
+                file_to_move = os.path.join(self.src_folder, filename)
+                shutil.move(file_to_move, dst_player_folder)
                 nr_files_moved += 1
         print(
-            f"\nFiles copied from {src_folder} to {dst_player_folder}: {nr_files_moved}"
+            f"\nFiles copied from {self.src_folder} to {dst_player_folder}: {nr_files_moved}"
         )
 
-    def _does_file_belong_to_player(self, filename):
+    def _belongs_to_player(self, filename):
         # uses serial because player-name is not present in file-name
         if self.player.serial in filename:
             return True
         else:
             return False
 
-    def _is_file_duplicate(filename):
+    def _is_duplicate(self, filename):
         # Define the regular expression pattern to match "(number)"
         pattern = r"\(\d+\)"
-
         # Search for the pattern in the filename
         match = re.search(pattern, filename)
-
         # If a match is found, return True; otherwise, return False
         return bool(match)
