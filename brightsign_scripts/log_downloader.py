cc98cd995917bbce94dbc46a0f80e64188d28f8a
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-23 00:01:58 +0200

    simplifying of variables and methods-names

--- brightsign_scripts/log_downloader.py
+++ brightsign_scripts/log_downloader.py
@@ -13,23 +13,23 @@ class LogDownloader:
 
     def download_logs(self):
         print(f"\nAttempting downloading logs from player: {self.player.name}")
+
         try:
-            json_data = self._json_from_url()
+            json_data = self._get_json()
         except requests.Timeout:
             print(
                 f"\nCould not connect to player with name: {self.player.name}"
             )
+
         try:
-            log_name_list = self._log_name_list_from_json(json_data)
+            log_names: list = self._get_log_names(json_data)
         except KeyError:
             print(f"\nNo logs found on player with name: {self.player.name}")
 
-        self._download_logs_from_list(
-            log_name_list, self.player.name
-        )  # STEMMER IKKE OVERENS MED IP I METHOD
+        self._open_download_url(log_names)
         print(f"\nDownload of player with name: {self.player.name} complete")
 
-    def _json_from_url(self):
+    def _get_json(self):
         device_url = f"http://{self.player.ip}/api/v1/files/sd/logs/"
         # gets json from url
         r = requests.get(device_url)
@@ -37,20 +37,21 @@ class LogDownloader:
         json_data = json.loads(t)
         return json_data
 
-    def _log_name_list_from_json(data):
-        # counts nr of log-entries
-        logs = data["data"]["result"]["files"]
+    def _get_log_names(json_data):
+        # collect log names from json
+        logs = json_data["data"]["result"]["files"]
         log_count = len(logs)
-        log_name_list = []
+        log_names = []
         x = 1
-        for x in range(log_count):
-            log_name_list.append(data["data"]["result"]["files"][x]["name"])
-            x += 1
-        return log_name_list
 
-    def _download_logs_from_list(self, log_name_list):
+        for x in range(log_count):
+            log_names.append(json_data["data"]["result"]["files"][x]["name"])
+            x += 1
+        return log_names
+
+    def _open_download_url(self, log_names):
         # url_eksempel: "http://10.0.1.115/api/v1/files/sd/logs/BrightSignLog.TKD1CN002225-220919000.log?contents&stream"
-        # Can't download through request. Uses webbrowser.open instead to open a download-window
-        for log_name in log_name_list:
+        # Can't download through request. Uses webbrowser.open to open a download link in browser
+        for log_name in log_names:
             download_url = f"http://{self.player.ip}/api/v1/files/sd/logs/{log_name}?contents&stream"
             webbrowser.open(download_url)
