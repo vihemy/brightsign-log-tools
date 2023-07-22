8bdc2b5008ca095a781bda361416f21d16be9d59
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-22 21:33:53 +0200

    simplified  names of methods and variables + begun refactoring of main

--- brightsign_scripts/log_mover.py
+++ brightsign_scripts/log_mover.py
@@ -9,32 +9,35 @@ from player import Player
 
 
 class LogMover:
-    def __init__(self, player: Player, source_path, destination_root):
+    def __init__(self, player: Player, src_folder, dst_parent_folder):
         self.player = player
-        self.source_path = source_path
-        self.destination_root = destination_root
+        self.src_folder = src_folder
+        self.dst_parent_folder = dst_parent_folder
 
-    def copy_logs_to_new_directory(self, source_path, destination_root):
-        destination_path = self._set_destination_path(destination_root)
-        utilities.create_directory(destination_path)
-        self._copy_unique_files_to_directory(source_path, destination_path)
+    def copy_logs_to_new_directory(self, src_folder, dst_parent_folder):
+        dst_player_folder = self._set_destination_player_folder(
+            dst_parent_folder
+        )
+        utilities.create_directory(dst_player_folder)
+        self._copy_unique_files_to_directory(src_folder, dst_player_folder)
 
-    def _set_destination_path(self, destination_root):
-        # sets destination folder using the player name
-        destination_path = destination_root + f"\{self.player.name}"
-        return destination_path
+    def _set_destination_player_folder(self, dst_parent_folder):
+        dst_player_folder = dst_parent_folder + f"\{self.player.name}"
+        return dst_player_folder
 
-    def _copy_unique_files_to_directory(self, source_path, destination_path):
+    def _copy_unique_files_to_directory(self, src_folder, dst_player_folder):
         nr_files_moved = 0
-        for filename in os.listdir(source_path):
+        for filename in os.listdir(src_folder):
             if (
                 self._does_file_belong_to_player == True
                 and self._is_file_duplicate(filename) == False
             ):
-                shutil.copy(os.path.join(source_path, filename), destination_path)
+                shutil.copy(
+                    os.path.join(src_folder, filename), dst_player_folder
+                )
                 nr_files_moved += 1
         print(
-            f"\nFiles copied from {source_path} to {destination_path}: {nr_files_moved}"
+            f"\nFiles copied from {src_folder} to {dst_player_folder}: {nr_files_moved}"
         )
 
     def _does_file_belong_to_player(self, filename):
--- brightsign_scripts/main.py
+++ brightsign_scripts/main.py
@@ -1,5 +1,6 @@
 import utilities
 from log_downloader import LogDownloader
+from log_mover import LogMover
 
 from loghandlers import logscraper_main
 from loghandlers import logdistributer_main
@@ -11,24 +12,19 @@ from loghandlers import ekspertskaerm2_main
 
 
 def main():
-    # ------------------------------------------------------ CONFIG ------------------------------------------------------
-    index_path = utilities.get_file_path_from_config("brightsign_index_path")
+    player_instances = get_player_instances()
+    # download_logs(player_instances)
+    move_logs(player_instances)
 
-    player_instances = utilities.get_player_instances_from_index(index_path)
-
-    # # ------------------------------------------------------ DOWNLOAD LOGS -----------------------------------------------------
-
-    for player in player_instances:
-        downloader = LogDownloader(player)
-        downloader.download_logs()
-
-    # # ------------------------------------------------------ MOVE LOGS ------------------------------------------------------
-    # log_download_folder = utilities.get_file_path_from_config("log_download_folder")
+    # # ------------------------------------------------------ Move LOGS ------------------------------------------------------
+    # log_source_directory = utilities.get_file_path_from_config(
+    #     "log_source_directory"
+    # )
     # log_destination_root_folder = utilities.get_file_path_from_config(
     #     "log_destination_root_folder"
     # )
 
-    # log_downloader(players, log_download_folder, log_destination_root_folder)
+    # log_downloader(players, log_source_directory, log_destination_root_folder)
 
     # # ------------------------------------------------------ ANALYZE LOGS ------------------------------------------------------
     # dvaergpingvin_main(log_destination_root_folder)
@@ -38,6 +34,31 @@ def main():
     # lyttestation_common_main(log_destination_root_folder, "Lyttestation 3")
     # ekspertskaerm1_main(log_destination_root_folder)
     # ekspertskaerm2_main(log_destination_root_folder)
+
+
+def get_player_instances():
+    index_path = utilities.get_file_path_from_config("brightsign_index_path")
+
+    player_instances = utilities.get_player_instances_from_index(index_path)
+
+    return player_instances
+
+
+def download_logs(player_instances):
+    for player in player_instances:
+        downloader = LogDownloader(player)
+        downloader.download_logs()
+
+
+def move_logs(player_instances):
+    src_folder = utilities.get_file_path_from_config("log_source_directory")
+    dst_parent_folder = utilities.get_file_path_from_config(
+        "log_destination_parent_folder"
+    )
+
+    for player in player_instances:
+        mover = LogMover(player, src_folder, dst_parent_folder)
+        mover.copy_logs_to_new_directory(src_folder, dst_parent_folder)
 
 
 if __name__ == "__main__":
