02c3ee7bea65d071d64d2ba11261f26994974835
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-26 22:44:24 +0200

    added docstrings

--- brightsign_scripts/log_downloader.py
+++ brightsign_scripts/log_downloader.py
@@ -8,18 +8,19 @@ from player import Player
 
 
 class LogDownloader:
+    """Class to handle downloading of logs from a BrightSign player using Brightsign's Diagnostic Web Server"""
+
     def __init__(self, player: Player):
         self.player = player
 
     def download_logs(self):
+        """Download all logs from a given BrightSign players log directory via Brightsign's Diagnostic Web Server"""
         print(f"\nAttempting downloading logs from player: {self.player.name}")
 
         try:
             json_data = self._get_json()
         except requests.Timeout:
-            print(
-                f"\nCould not connect to player with name: {self.player.name}"
-            )
+            print(f"\nCould not connect to player with name: {self.player.name}")
 
         try:
             log_names: list = self._get_log_names(json_data)
@@ -30,15 +31,15 @@ class LogDownloader:
         print(f"\nDownload of player with name: {self.player.name} complete")
 
     def _get_json(self):
+        """Return json object containing given Brightsign player's log info from Diagnostic Web Server"""
         device_url = f"http://{self.player.ip}/api/v1/files/sd/logs/"
-        # gets json from url
         r = requests.get(device_url)
         t = r.text
         json_data = json.loads(t)
         return json_data
 
     def _get_log_names(json_data):
-        # collect log names from json
+        """Return a list of log names from given json object"""
         logs = json_data["data"]["result"]["files"]
         log_count = len(logs)
         log_names = []
@@ -50,6 +51,7 @@ class LogDownloader:
         return log_names
 
     def _open_download_url(self, log_names):
+        """Open a download url for each log in given list of log names"""
         # url_eksempel: "http://10.0.1.115/api/v1/files/sd/logs/BrightSignLog.TKD1CN002225-220919000.log?contents&stream"
         # Can't download through request. Uses webbrowser.open to open a download link in browser
         for log_name in log_names:
