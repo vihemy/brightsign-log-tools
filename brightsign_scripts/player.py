6ec95e20709e944860cd444245f3d864e9f227c4
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-26 23:02:52 +0200

    added docstrings

--- brightsign_scripts/player.py
+++ brightsign_scripts/player.py
@@ -1,4 +1,19 @@
 class Player:
+    """
+    A class to represent a BrightSign player
+    
+    Attributes
+    ----------
+    name : str
+        the name of the player
+    ip : str
+        the ip address of the player
+    serial : str
+        the serial number of the player
+    searchwords : str
+        the searchwords, used in anlysing the player's logs
+    """
+
     def __init__(self, name, ip, serial, searchwords):
         self.name = name
         self.ip = ip
