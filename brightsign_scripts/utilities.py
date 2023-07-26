93aa3a5a6a3f4e9f6739410e1a8982092d3d70fe
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-26 22:57:08 +0200

    added docstrings to all functions

--- brightsign_scripts/utilities.py
+++ brightsign_scripts/utilities.py
@@ -9,17 +9,19 @@ import pandas as pd
 from player import Player
 
 
-def get_file_path_from_config(file_name):
+def get_file_path_from_config(config_key):
+    """Return a file path from a given key in config.ini."""
     this_file = Path(__file__)
     ROOT_DIR = this_file.parent.parent.absolute()
     config_path = os.path.join(ROOT_DIR, "config.ini")
     parser = ConfigParser()
     parser.read(config_path)
-    file_path = parser.get("file_paths", file_name)
+    file_path = parser.get("file_paths", config_key)
     return file_path
 
 
 def get_date():
+    """Return today's date in danish format."""
     # converts to danish date-format
     today = date.today()
     today = today.strftime("%d-%m-%Y")
@@ -27,18 +29,20 @@ def get_date():
 
 
 def shorten_filename(filename):
+    """Return a shortened version of a given filename (using the final component of the path)."""
     # shortens filename for convience
     filenameShort = os.path.basename(filename)
     return filenameShort
 
 
 def create_directory(path):
-    # Creates new folder if doesn't exist
+    """Create new path if doesn't exist."""
     if not os.path.exists(path):
         os.makedirs(path)
 
 
 def get_player_instances_from_index(index_path):
+    """Return a list of instances of type Player from a given index file."""
     df = pd.read_excel(
         str(index_path),
         usecols=["Navn", "IP-adresse", "Serienummer", "Søgeord"],
