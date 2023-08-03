adfd4b83fc752fa590f3e7cfe7c7aa0047c4d98a
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-08-03 21:02:13 +0200

    not finished

--- brightsign_scripts/main.py
+++ brightsign_scripts/main.py
@@ -1,17 +1,19 @@
 import utilities
 from log_downloader import LogDownloader
 from log_mover import LogMover
+from log_analyzer import LogAnalyzer
 
 
 def main():
     player_instances = get_player_instances()
-    download_logs(player_instances)
-    move_logs(player_instances)
+    # download_logs(player_instances)
+    # move_logs(player_instances)
+    analyze_logs(player_instances)
 
 
 def get_player_instances():
     """Return a list of instances of type Player using player-data found through config.ini."""
-    index_path = utilities.get_file_path_from_config("brightsign_index_path")
+    index_path = utilities.get_path_from_config("brightsign_index_path")
     player_instances = utilities.get_player_instances_from_index(index_path)
     return player_instances
 
@@ -25,14 +27,21 @@ def download_logs(player_instances):
 
 def move_logs(player_instances):
     """Move all logs from a given list of BrightSign players to a destination directory configured in config.ini"""
-    src_folder = utilities.get_file_path_from_config("log_source_directory")
-    dst_parent_folder = utilities.get_file_path_from_config(
-        "log_destination_parent_folder"
-    )
+    src_folder = utilities.get_path_from_config("log_source_directory")
+    dst_parent_folder = utilities.get_path_from_config("log_destination_parent_folder")
 
     for player in player_instances:
         mover = LogMover(player, src_folder, dst_parent_folder)
         mover.relocate_logs()
+
+
+def analyze_logs(player_instances):
+    """Use LogAnalyzer-class to analyze logs belonging to each player-instance on a list and create a CSV file with the total count of occurrences + each search word"""
+    log_parent_folder = utilities.get_path_from_config("log_destination_parent_folder")
+
+    for player in player_instances:
+        analyzer = LogAnalyzer(player, log_parent_folder)
+        analyzer.analyze_logs_and_generate_csv()
 
 
 if __name__ == "__main__":
