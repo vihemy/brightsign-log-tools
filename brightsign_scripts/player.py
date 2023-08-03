e0a0b6d954e0e7ce8b52b0b9e5d62c80da4255dc
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-08-03 21:01:11 +0200

    updated type hints

--- brightsign_scripts/player.py
+++ brightsign_scripts/player.py
@@ -14,8 +14,8 @@ class Player:
         the searchwords, used in anlysing the player's logs
     """
 
-    def __init__(self, name, ip, serial, searchwords):
-        self.name: str = name
-        self.ip: str = ip
-        self.serial: str = serial
-        self.searchwords: str = searchwords
+    def __init__(self, name: str, ip: str, serial: str, searchwords: str):
+        self.name = name
+        self.ip= ip
+        self.serial= serial
+        self.searchwords= searchwords
