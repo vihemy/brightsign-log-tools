5bc082ba0f21153c5d78bd8ed255113fdcda64f6
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-08-16 15:41:53 +0200

    added _download_to_folder-method, which uses requests instead of webbrowser.open

--- brightsign_scripts/log_downloader.py
+++ brightsign_scripts/log_downloader.py
@@ -5,6 +5,7 @@ import webbrowser
 
 # Internal modules
 from player import Player
+from utilities import get_path_from_config
 
 
 class LogDownloader:
@@ -42,7 +43,7 @@ class LogDownloader:
             return
         
         try:
-            self._open_download_url(log_names)
+            self._download_to_folder(log_names)
         except UnboundLocalError as err:
             print(err)
             return
@@ -69,10 +70,27 @@ class LogDownloader:
             x += 1
         return log_names
 
-    def _open_download_url(self, log_names):
-        """Open a download url for each log in given list of log names"""
+    def _download_to_folder(self, log_names):
+        """Open a download url for each log in given list of log names using requests and write to file. (Slower than opening a download url in browser, but doesn't open webbrowser)"""
         # url_eksempel: "http://10.0.1.115/api/v1/files/sd/logs/BrightSignLog.TKD1CN002225-220919000.log?contents&stream"
-        # Can't download through request. Uses webbrowser.open to open a download link in browser
+
+        for log_name in log_names:
+            log_file_path = self._create_log_file_path(log_name)
+
+            url = f"http://{self.player.ip}/api/v1/files/sd/logs/{log_name}?contents&stream"
+            r = requests.get(url, allow_redirects=True)
+            open(log_file_path, "wb").write(r.content)
+
+    def _create_log_file_path(self, log_name):
+        """Create and return a file path for a given log name"""
+        log_parent_folder = get_path_from_config("log_destination_parent_folder")
+        log_file_path = log_parent_folder + f"/{self.player.name}" + f"/{log_name}"
+        return log_file_path
+    
+    def _open_download_url(self, log_names):
+        """Open a download url for each log in given list of log names. (Faster than downloading to folder with requests, but opens a webbrowser to do it.)"""
+        # url_eksempel: "http://10.0.1.115/api/v1/files/sd/logs/BrightSignLog.TKD1CN002225-220919000.log?contents&stream"
+        # Uses webbrowser.open to open a download link in browser
         for log_name in log_names:
             download_url = f"http://{self.player.ip}/api/v1/files/sd/logs/{log_name}?contents&stream"
             webbrowser.open(download_url)
