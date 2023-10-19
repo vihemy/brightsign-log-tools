c257d1a12456a8c0b26e56661dc02da6da5946ca
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-10-19 17:09:24 +0200

    add write_to_csv and write_to_xlsx

--- scripts/log_analyzer.py
+++ scripts/log_analyzer.py
@@ -5,42 +5,37 @@ import pandas as pd
 
 # Internal modules
 from player import Player
-from utilities import get_date
-from utilities import get_data_from_config
-from utilities import create_directory
+from utilities import get_date2, get_data_from_config, create_directory
 
 
 class LogAnalyzer:
     def __init__(self, player: Player):
         self.player = player
-        self.log_directory = self.get_log_directory()
+        self.log_parent_dir = get_data_from_config("file_paths", "log_parent_folder")
+        self.log_dir = self.get_log_dir()
         self.log_names = self._get_log_names()
         self.log_paths = self._get_log_paths()
 
-    def get_log_directory(self):
+    def get_log_dir(self):
         """Return directory containing the log files to analyze."""
-        log_directory = os.path.join(
-            get_data_from_config("file_paths", "log_parent_folder"), self.player.name
-        )
-        return log_directory
+        log_dir = os.path.join(self.log_parent_dir, self.player.name)
+        return log_dir
 
     def _get_log_names(self):
         log_names = [
             filename
-            for filename in os.listdir(self.log_directory)
+            for filename in os.listdir(self.log_dir)
             if filename.endswith(".log")
         ]
         return log_names
 
     def _get_log_paths(self):
         log_paths = [
-            os.path.join(self.log_directory, log_name) for log_name in self.log_names
+            os.path.join(self.log_dir, log_name) for log_name in self.log_names
         ]
         return log_paths
 
-    # ----------------------------------------------- RYD OP HERUNDER + LAV CSV EXPORT --------------------------------------------------#
-
-    def analyze_logs_and_generate_csv(self):
+    def analyze_logs_and_export(self):
         data = self._create_data()
         for log_name in self.log_names:
             # Open log file and read content
@@ -54,6 +49,8 @@ class LogAnalyzer:
 
         df = self._convert_data_to_df(data)
         print(df)
+        player_export_dir = self._create_player_export_folder()
+        self._write_csv_file(player_export_dir, df)
 
     def _create_data(self):
         data = {"Date": []}
@@ -62,7 +59,7 @@ class LogAnalyzer:
         return data
 
     def _read_log_content(self, log_name):
-        log_file_path = os.path.join(self.log_directory, log_name)
+        log_file_path = os.path.join(self.log_dir, log_name)
         with open(log_file_path, "r") as log_file:
             log_content = log_file.read()
         return log_content
@@ -96,51 +93,31 @@ class LogAnalyzer:
                 data[sw] = [counts[i]]
 
     def _convert_data_to_df(self, data):
-        # Create dataframe from data dict
         df = pd.DataFrame(data)
-        # Group by date and sum
-        df.groupby("Date").sum()
+        df = df.groupby("Date").sum()
         # Add total row
         df.loc["Total"] = df.sum(numeric_only=True)
         # Convert searchword columns to int
         df[self.player.searchword_list] = df[self.player.searchword_list].astype(int)
         return df
 
-    # def _add_count_to_df(self, log_content):
-    #     for searchword in self.player.searchword_list:
-    #         count = len(re.findall(searchword, log_content, re.IGNORECASE))
+    def _create_player_export_folder(self):
+        base_export_dir = "exports"
+        player_export_dir = self.player.name
+        folder_path = os.path.join(
+            self.log_parent_dir, base_export_dir, player_export_dir
+        )
+        create_directory(folder_path)
+        return folder_path
 
-    #     df = self._count_searchwords(log_content)
-    #     total_count = sum(df.values())
-    #     self._add_to_df(log_name, total_count, df)
-    #     self.total_count += total_count
+    def _write_csv_file(self, player_export_dir, df: pd.DataFrame):
+        file_name = f"{self.player.name}_export_{get_date2()}.csv"
+        file_path = os.path.join(player_export_dir, file_name)
+        df.to_csv(file_path, index=False)
+        print(f"CSV file '{file_path}' generated successfully.")
 
-    # for each log in log_names, count the occurences of each item in searchword_list and add it to the dataframe under the corresponding header
-
-    #     self._write_csv_file()
-
-    # def _count_searchwords(self, log_content):
-    #     df = {}
-    #     for sw in self.player.searchword_list:
-    #         count = len(re.findall(sw.strip(), log_content, re.IGNORECASE))
-    #         df[sw.strip()] = count
-    #     return df
-
-    # def _add_to_df(self, log_file, total_count, df):
-    #     data = {"Log File": log_file, "Total Count": total_count, **df}
-    #     self.df = self.df.append(data, ignore_index=True)
-
-    # def _write_csv_file(self):
-    #     csv_filepath = self._create_filepath()
-    #     self.df.to_csv(csv_filepath, index=False)
-    #     print(f"CSV file '{csv_filepath}' generated successfully.")
-
-    # def _create_filepath(self):
-    #     player_export_folder = os.path.join(
-    #         get_data_from_config("export_parent_folder"),
-    #         self.player.name + " export",
-    #     )
-    #     create_directory(player_export_folder)
-    #     file_name = f"{self.player.name}_export_{get_date()}.csv"
-    #     file_path = os.path.join(player_export_folder, file_name)
-    #     return file_path
+    def _write_xlsx_file(self, player_export_dir, df: pd.DataFrame):
+        file_name = f"{self.player.name}_export_{get_date2()}.xlsx"
+        file_path = os.path.join(player_export_dir, file_name)
+        df.to_csv(file_path, index=False)
+        print(f"XLSX file '{file_path}' generated successfully.")
