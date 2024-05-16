5cc1ac9537d19db4f59d6b076b1c64f39d26fa85
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-05-16 15:25:47 +0200

    compile regex, added whitespacetrim and replaced comments with docstrings

--- scripts/log_aggregator.py
+++ scripts/log_aggregator.py
@@ -22,35 +22,33 @@ headers = [
     "PreviousEventData",
 ]
 
+FILENAME_PATTERN = re.compile(r".*BrightSignLog\.(\w+)-(\d{6}).*\.log")
+
 
 def get_processed_files(log_file):
+    """Retrieves set of previously processed file names from a log file."""
+    processed_files = set()
     if os.path.exists(log_file):
         with open(log_file, "r") as file:
             processed_files = {line.strip() for line in file}
-    else:
-        processed_files = set()
     return processed_files
 
 
-# Function to extract player serial number and date from the file name
 def extract_info_from_filename(filename):
-    pattern = r".*BrightSignLog\.(\w+)-(\d{6}).*\.log"
-    match = re.search(pattern, filename)
+    """Extracts player serial number and log date from filename."""
+    match = FILENAME_PATTERN.search(filename)
     if match:
-        serial_number = match.group(1)
-        log_date = match.group(2)
-        return serial_number, log_date
+        return match.group(1), match.group(2)
     return None, None
 
 
-# Function to parse logs from a single file and append to the csv writer
 def parse_log_file(file_path, csv_writer):
+    """Parses a log file and processes different types of log entries."""
     serial_number, log_date = extract_info_from_filename(os.path.basename(file_path))
     if not serial_number:
         return  # Skip if the filename pattern does not match
 
     with open(file_path, "r") as file:
-
         for line in file:
             line = line.strip()
             if "L=p" in line:
@@ -61,8 +59,8 @@ def parse_log_file(file_path, csv_writer):
                 process_state_log(line, csv_writer, serial_number, log_date)
 
 
-# Processing playback logs
 def process_playback_log(line, csv_writer, serial_number, log_date):
+    """Processes playback logs and writes data to a CSV file."""
     data = {
         "PlayerSerialNumber": serial_number,
         "LogType": "Playback",
@@ -91,8 +89,8 @@ def process_playback_log(line, csv_writer, serial_number, log_date):
     csv_writer.writerow(data)
 
 
-# Processing event logs
 def process_event_log(line, csv_writer, serial_number, log_date):
+    """Processes event logs and writes data to a CSV file."""
     data = {
         "PlayerSerialNumber": serial_number,
         "LogType": "Event",
@@ -116,8 +114,8 @@ def process_event_log(line, csv_writer, serial_number, log_date):
     csv_writer.writerow(data)
 
 
-# Processing state logs
 def process_state_log(line, csv_writer, serial_number, log_date):
+    """Processes state logs and writes data to a CSV file."""
     data = {
         "PlayerSerialNumber": serial_number,
         "LogType": "State",
@@ -132,6 +130,7 @@ def process_state_log(line, csv_writer, serial_number, log_date):
     for field in fields:
         if "=" in field:
             key, value = field.split("=", 1)
+            value = value.strip()
             if key == "S":
                 data["StateName"] = value
             elif key == "T":
@@ -148,17 +147,18 @@ def process_state_log(line, csv_writer, serial_number, log_date):
 
 
 def update_processed_files(log_file, filename):
+    """Updates log file with new entries of processed files."""
     with open(log_file, "a") as file:
         file.write(filename + "\n")
 
 
-# Main function to orchestrate log parsing and CSV writing
 def main(log_directory: str, output_csv_path: str, processed_log_file: str):
+    """Orchestrates log parsing and CSV writing for an entire directory of log files."""
     processed_files = get_processed_files(processed_log_file)
 
-    with open(output_csv_path, "a", newline="") as csv_file:  # Open in append mode
+    with open(output_csv_path, "a", newline="") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=headers)
-        if os.stat(output_csv_path).st_size == 0:  # If file is empty, write header
+        if os.stat(output_csv_path).st_size == 0:
             writer.writeheader()
 
         for dirpath, dirnames, filenames in os.walk(log_directory):
@@ -171,8 +171,10 @@ def main(log_directory: str, output_csv_path: str, processed_log_file: str):
     print("Log aggregation completed.")
 
 
+# Load configuration settings
 log_parent_folder = get_data_from_config("file_paths", "log_parent_folder")
 aggregated_log_file = get_data_from_config("file_paths", "aggregated_logs_path")
 processed_logs = get_data_from_config("file_paths", "processed_logs_path")
 
+# Uncomment the following line to run the main function
 main(log_parent_folder, aggregated_log_file, processed_logs)
